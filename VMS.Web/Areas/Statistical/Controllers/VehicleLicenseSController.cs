6c6d46896f8067e61062c102d12a5759ed47aca4
xiaoistone <oxf5deb3@163.com>
2018-10-10 21:52:18 +0800

    补提上次未提交的代码

---
+++ VMS.Web/Areas/Statistical/Controllers/VehicleLicenseSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMS.Areas.Statistical.Controllers
+{
+    public class VehicleLicenseSController : Controller
+    {
+        // GET: Statistical/VehicleLicenseS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ VSM.DTO/Statistical/pieDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMS.DTO.Statistical
+{
+    public class pieDTO
+    {
+        public String permitted_card_type_no { get; set; }
+
+        public String permitted_card_type_count { get; set; }
+    }
+}
