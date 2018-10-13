a8f4c18fbb717ffdc04c15bc22e2fdea62e0e683
xiaoistone <oxf5deb3@163.com>
2018-10-13 20:20:14 +0800

    打印 优化

--- VMS.Services/DriverLicense/DriverLicenseService.cs
+++ VMS.Services/DriverLicense/DriverLicenseService.cs
@@ -697,7 +697,7 @@ namespace VMS.Services
       + ",[status],[oper_date] ,[oper_id],[modify_date],[modify_oper_id] ,[time_stamp]"
       + ",[user_photo_path],[car_1_img_path],[car_2_img_path],[engine_no_img_path],[vin_no_img_path]"
      + ",[start_date],[sex],[car_number],[end_date] ,[name],[nation],[passenger],b.region_name, a.region_no "
-  + "FROM [VehicleManagementSystem].[dbo].[t_normal_car_license] a "
+  + "FROM [dbo].[t_normal_car_license] a "
   + "left join t_bd_region b on a.region_no=b.region_no"
   + " where 1=1 " + param
     + ")as oo) as o where rownumber>=" + ((index - 1) * pageSize + 1)+";";
@@ -736,7 +736,7 @@ namespace VMS.Services
      +"  ,[car_2_img_path],[engine_no_img_path],[vin_no_img_path],[img4_url],[img5_url],[car_type] " 
       +" ,[temp_number],[engine_no],[vin],[passenger],[cargo],[label_type],[id_no],[id_card] "
      + "  ,[start_date],[end_date],[addr],c.type_name as permitted_card_type_name,a.region_no, a.permitted_car_type_no as permitted_card_type_no "
-  +" FROM [VehicleManagementSystem].[dbo].[t_temp_car_license] a  "
+  +" FROM [dbo].[t_temp_car_license] a  "
   +" left join t_bd_region b on a.region_no=b.region_no "
   +" left join t_bd_permitted_car_type c on a.permitted_car_type_no=c.type_no "
    +" where 1=1  "
